0554e7901ed3f8540f1cb1e2bb9ea074ec00a56c
Isaac Adams <huskydev@pm.me>
2022-06-13 20:16:38 -0400

    Handling all WF Message types for Encoding (#4)
    * this should handle all message types for encoding
    
    * lowercase

--- src/wf_models/mod.rs
+++ src/wf_models/mod.rs
@@ -1,38 +1,7 @@
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use std::collections::HashMap;
-
 #[cfg(test)]
 mod test;
 
-mod authentication;
-mod header;
+mod deserialize;
 mod serialize;
 
-pub use authentication::AuthenticationMessage;
-pub use header::MessageHeader;
-
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all(deserialize = "camelCase"))]
-pub struct WhiteflagMessage {
-    #[serde(flatten)]
-    pub header: MessageHeader,
-    #[serde(flatten)]
-    pub body: HashMap<String, Value>,
-}
-
-impl TryFrom<WhiteflagMessage> for Vec<String> {
-    type Error = String;
-
-    fn try_from(message: WhiteflagMessage) -> Result<Self, Self::Error> {
-        let values: Vec<String> = match &message.header.message_code {
-            'A' => {
-                let auth: AuthenticationMessage = message.try_into()?;
-                auth.into()
-            }
-            _ => vec![],
-        };
-
-        Ok(values)
-    }
-}
+pub use deserialize::WhiteflagFieldValues;
--- src/wf_models/test.rs
+++ src/wf_models/test.rs
@@ -1,8 +1,8 @@
-use super::{AuthenticationMessage, WhiteflagMessage};
+use crate::{decode_from_hex, wf_models::deserialize::WhiteflagFieldValues};
 use serde_json::json;
 
 #[test]
-fn serialize_authentication_message() {
+fn deserialize_message_from_json_to_fields() {
     let prefix = "WF";
     let version = "1";
     let encryption_indicator = "0";
@@ -26,96 +26,46 @@ fn serialize_authentication_message() {
     })
     .to_string();
 
-    let auth_message: AuthenticationMessage = serde_json::from_str(&json).unwrap();
+    let message: WhiteflagFieldValues = serde_json::from_str(&json).unwrap();
 
-    assert_eq!(prefix, auth_message.header.prefix);
-    assert_eq!(version, auth_message.header.version);
     assert_eq!(
-        encryption_indicator,
-        auth_message.header.encryption_indicator
-    );
-    assert_eq!(duress_indicator, auth_message.header.duress_indicator);
-    assert_eq!(message_code, auth_message.header.message_code);
-    assert_eq!(reference_indicator, auth_message.header.reference_indicator);
-    assert_eq!(referenced_message, auth_message.header.referenced_message);
-    assert_eq!(verification_method, auth_message.verification_method);
-    assert_eq!(verification_data, auth_message.verification_data);
+        vec![
+            prefix,
+            version,
+            encryption_indicator,
+            duress_indicator,
+            &message_code.to_string(),
+            reference_indicator,
+            referenced_message,
+            verification_method,
+            verification_data
+        ],
+        message.fields
+    )
 }
 
 #[test]
-fn serialize_message() {
-    let prefix = "WF";
-    let version = "1";
-    let encryption_indicator = "0";
-    let duress_indicator = "0";
-    let message_code = 'A';
-    let reference_indicator = "0";
-    let referenced_message = "0000000000000000000000000000000000000000000000000000000000000000";
-    let verification_method = "1";
-    let verification_data = "https://organisation.int/whiteflag";
+fn test_serialize_auth_message_from_hex() {
+    let hex = "5746313020800000000000000000000000000000000000000000000000000000000000000000b43a3a38399d1797b7b933b0b734b9b0ba34b7b71734b73a17bbb434ba32b33630b380";
 
     let json = json!({
-        "prefix": prefix,
-        "version": version,
-        "encryptionIndicator": encryption_indicator,
-        "duressIndicator": duress_indicator,
-        "messageCode": message_code,
-        "referenceIndicator": reference_indicator,
-        "referencedMessage": referenced_message,
-        "verificationMethod": verification_method,
-        "verificationData": verification_data
+        "prefix": "WF",
+        "version": "1",
+        "encryptionIndicator": "0",
+        "duressIndicator": "0",
+        "messageCode": 'A',
+        "referenceIndicator": "0",
+        "referencedMessage": "0000000000000000000000000000000000000000000000000000000000000000",
+        "verificationMethod": "1",
+        "verificationData": "https://organisation.int/whiteflag"
     })
     .to_string();
 
-    let message: WhiteflagMessage = serde_json::from_str(&json).unwrap();
-    let wf_message: AuthenticationMessage = message.try_into().unwrap();
-
-    assert_eq!(prefix, wf_message.header.prefix);
-    assert_eq!(version, wf_message.header.version);
-    assert_eq!(encryption_indicator, wf_message.header.encryption_indicator);
-    assert_eq!(duress_indicator, wf_message.header.duress_indicator);
-    assert_eq!(message_code, wf_message.header.message_code);
-    assert_eq!(reference_indicator, wf_message.header.reference_indicator);
-    assert_eq!(referenced_message, wf_message.header.referenced_message);
-    assert_eq!(verification_method, wf_message.verification_method);
-    assert_eq!(verification_data, wf_message.verification_data);
+    test_json(&json, &decode_from_hex(hex).unwrap());
 }
 
-#[test]
-fn serialize_message_into_values() {
-    let prefix = "WF";
-    let version = "1";
-    let encryption_indicator = "0";
-    let duress_indicator = "0";
-    let message_code = 'A';
-    let reference_indicator = "0";
-    let referenced_message = "0000000000000000000000000000000000000000000000000000000000000000";
-    let verification_method = "1";
-    let verification_data = "https://organisation.int/whiteflag";
-
-    let json = json!({
-        "prefix": prefix,
-        "version": version,
-        "encryptionIndicator": encryption_indicator,
-        "duressIndicator": duress_indicator,
-        "messageCode": message_code,
-        "referenceIndicator": reference_indicator,
-        "referencedMessage": referenced_message,
-        "verificationMethod": verification_method,
-        "verificationData": verification_data
-    })
-    .to_string();
-
-    let message: WhiteflagMessage = serde_json::from_str(&json).unwrap();
-    let values: Vec<String> = message.try_into().unwrap();
-
-    assert_eq!(prefix, values[0]);
-    assert_eq!(version, values[1]);
-    assert_eq!(encryption_indicator, values[2]);
-    assert_eq!(duress_indicator, values[3]);
-    assert_eq!(message_code.to_string(), values[4]);
-    assert_eq!(reference_indicator, values[5]);
-    assert_eq!(referenced_message, values[6]);
-    assert_eq!(verification_method, values[7]);
-    assert_eq!(verification_data, values[8]);
+fn test_json(actual: &str, expected: &str) {
+    let a = serde_json::from_str::<serde_json::Map<String, serde_json::Value>>(actual).unwrap();
+    let e = serde_json::from_str::<serde_json::Map<String, serde_json::Value>>(expected).unwrap();
+    assert_eq!(a, e);
 }
