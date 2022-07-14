778ef40bb5c703f43e1c34392371ddec98025da0
Husky <huskydev@pm.me>
2022-07-14 19:56:41 -0400

    moving methods into more appropriate places

--- src/wf_core/segment.rs
+++ src/wf_core/segment.rs
@@ -1,17 +1,4 @@
-use crate::wf_field::{FIELD_MESSAGETYPE, Field, FieldDefinition, generic_header_fields, get_body_from_code};
-
-pub fn get_message_code(header: &MessageSegment) -> char {
-    match header.get(&FIELD_MESSAGETYPE) {
-        Some(x) => x.chars().next(),
-        _ => None,
-    }
-    .expect("expected message code but none was found")
-}
-
-pub fn get_message_body(header: &MessageSegment) -> (MessageSegment, char) {
-    let message_code = get_message_code(header);
-    (MessageSegment::from_code(&message_code), message_code)
-}
+use crate::{wf_buffer::WhiteflagBuffer, wf_field::Field};
 
 #[derive(Clone)]
 pub struct MessageSegment {
@@ -21,58 +8,6 @@ pub struct MessageSegment {
 impl MessageSegment {
     pub fn from(fields: Vec<Field>) -> MessageSegment {
         MessageSegment { fields }
-    }
-
-    pub fn generic_header_segment() -> MessageSegment {
-        MessageSegment::from(generic_header_fields().to_vec())
-    }
-
-    /*
-     * Sets all field values of this segment with values from an array
-     * @since 1.1
-     * @param data array with the data to be set as the field values
-     * @param startIndex starting position in the array
-     * @return TRUE if the data was valid and all field values are set
-     * @throws WfCoreException if the provided data is invalid
-     */
-    pub fn set_all<T: AsRef<str> + Into<String>>(&mut self, data: &[T], start_index: usize) {
-        /* int nItems = data.length - startIndex;
-        if (nItems < fields.length) {
-            throw new WfCoreException("Message segment has " + fields.length + " fields, but received " + nItems + " items in array", null);
-        } */
-        let mut index = start_index;
-        for field in &mut self.fields {
-            /* if (Boolean.FALSE.equals(field.set(data[index]))) {
-                throw new WfCoreException("Field " + field.debugInfo() + " already set or array item " + index + " contains invalid data: " + data[index], null);
-            } */
-            let value = &data[index];
-            match field.set(value.as_ref()) {
-                Ok(_) => println!("Message field set successfully."),
-                Err(e) => panic!("{:?}", e),
-            }
-            index += 1;
-        }
-
-        //return this.isValid();
-    }
-
-    /**
-     * Gets the value of the field specified by name
-     * @param fieldname the name of the requested field
-     * @return the field value, or NULL if field does not exist
-     */
-    pub fn get<T: AsRef<str>>(&self, field_name: T) -> Option<&String> {
-        let value = self
-            .fields
-            .iter()
-            .find(|f| f.definition.name == field_name.as_ref())?
-            .get();
-
-        value.as_ref()
-    }
-
-    pub fn get_number_of_fields(&self) -> usize {
-        self.fields.len()
     }
 
     /**
@@ -95,50 +30,6 @@ impl MessageSegment {
         }
 
         buffer.into()
-    }
-
-    /**
-     * Decodes this message segment from the provided encoded message
-     * @since 1.1
-     * @param buffer the binary buffer with the binary encoded message
-     * @param startBit the bit position where this segment starts in the encoded message
-     * @param fieldIndex the index of the next field to be decoded
-     * @throws WfCoreException if the message cannot be decoded
-     */
-    pub fn decode(
-        &mut self,
-        message_buffer: &[u8],
-        message_buffer_bit_length: usize,
-        start_bit: usize,
-        field_index: usize,
-    ) -> usize {
-        /* Check if all fields already processed */
-        if field_index > self.fields.len() {
-            return 0;
-        }
-
-        let mut bit_cursor = start_bit;
-        let mut byte_cursor = self.fields[field_index].definition.start_byte;
-        for field in &mut self.fields[field_index..] {
-            if field.definition.start_byte != byte_cursor {
-                panic!("start byte should match byte cursor");
-                //throw new WfCoreException("Invalid field order while decoding: did not expect field " + fields[index].debugInfo() + " at byte " + byteCursor, null);
-            }
-            /*
-            try {
-                buffer.extractMessageField(fields[index], bitCursor);
-            } catch (WfCoreException e) {
-                throw new WfCoreException("Could not decode field at bit " + bitCursor + " of buffer: " + buffer.toHexString(), e);
-            } */
-
-            let bit_length =
-                field.extract_message_field(message_buffer, message_buffer_bit_length, bit_cursor);
-
-            bit_cursor += bit_length; //field.bit_length();
-            byte_cursor = field.definition.end_byte as usize;
-        }
-
-        bit_cursor - start_bit
     }
 
     /**
