461d50abe8d6d7014313b955e720976bfb1ccfd8
Ed Hertzog <edhertzog@gmail.com>
2022-07-17 00:24:23 -0400

    est_extract_field_hex()

--- src/wf_field/test.rs
+++ src/wf_field/test.rs
@@ -1,4 +1,4 @@
-use crate::wf_codec::encoding::{UTF8, BIN, DEC};
+use crate::wf_codec::encoding::{UTF8, BIN, DEC, HEX};
 use crate::{wf_buffer::WhiteflagBuffer, wf_field::Field, wf_field::FieldDefinition};
 use crate::wf_buffer::common::{to_hex};
 
@@ -55,15 +55,15 @@ fn test_extract_field_utf() {
 #[test]
 fn test_add_field_bin_1() {
     let buffer: Vec<u8> = vec![];
-    let mut buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 0); 
+    let mut wf_buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 0); 
     let mut field = Field::new(FIELDNAME, None, BIN, 0, -1);
 
     let success = field.set("01");
     assert!(success.is_ok());
 
-    buffer.append_field(&field);
+    wf_buffer.append_field(&field);
 
-    let (size, _) = buffer.extract_message_field(FieldDefinition::new(FIELDNAME, None, BIN, 0, -1), 0);
+    let (size, _) = wf_buffer.extract_message_field(FieldDefinition::new(FIELDNAME, None, BIN, 0, -1), 0);
 
     assert_eq!(
         field.bit_length(), 
@@ -71,7 +71,7 @@ fn test_add_field_bin_1() {
         "Buffer bit length should be equal to field bit length"
     );
 
-    let (result, _) = From::from(buffer);
+    let (result, _) = From::from(wf_buffer);
 
     assert_eq!(
         "40", 
@@ -83,15 +83,15 @@ fn test_add_field_bin_1() {
 #[test]
 fn test_add_field_bin_2() {
     let buffer: Vec<u8> = vec![];
-    let mut buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 0); 
+    let mut wf_buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 0); 
     let mut field = Field::new(FIELDNAME, None, BIN, 0, 3);
 
     let success = field.set("101");
     assert!(success.is_ok());
 
-    buffer.append_field(&field);
+    wf_buffer.append_field(&field);
 
-    let (size, _) = buffer.extract_message_field(FieldDefinition::new(FIELDNAME, None, BIN, 0, -1), 0);
+    let (size, _) = wf_buffer.extract_message_field(FieldDefinition::new(FIELDNAME, None, BIN, 0, -1), 0);
 
     assert_eq!(
         field.bit_length(), 
@@ -99,7 +99,7 @@ fn test_add_field_bin_2() {
         "Buffer bit length should be equal to field bit length"
     );
 
-    let (result, _) = From::from(buffer);
+    let (result, _) = From::from(wf_buffer);
 
     assert_eq!(
         "a0", 
@@ -111,15 +111,15 @@ fn test_add_field_bin_2() {
 #[test]
 fn test_add_field_dec() {
     let buffer: Vec<u8> = vec![];
-    let mut buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 0); 
+    let mut wf_buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 0); 
     let mut field = Field::new(FIELDNAME, None, DEC, 0, 4);
 
     let success = field.set("1478");
     assert!(success.is_ok());
 
-    buffer.append_field(&field);
+    wf_buffer.append_field(&field);
 
-    let (size, _) = buffer.extract_message_field(FieldDefinition::new(FIELDNAME, None, DEC, 0, -1), 0);
+    let (size, _) = wf_buffer.extract_message_field(FieldDefinition::new(FIELDNAME, None, DEC, 0, -1), 0);
 
     assert_eq!(
         field.bit_length(), 
@@ -127,7 +127,7 @@ fn test_add_field_dec() {
         "Buffer bit length should be equal to field bit length"
     );
 
-    let (result, _) = From::from(buffer);
+    let (result, _) = From::from(wf_buffer);
 
     assert_eq!(
         "1478", 
@@ -139,15 +139,70 @@ fn test_add_field_dec() {
 #[test]
 fn test_extract_field_dec() {
     let buffer: Vec<u8> = vec![0x95, 0x91, 0xFF, 0xE7];
-    let mut buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 0); 
-    let mut field = Field::new(FIELDNAME, None, DEC, 0, 2);
+    let wf_buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 32); 
+    //let field = Field::new(FIELDNAME, None, DEC, 0, 2);
 
-    let (size, _) = buffer.extract_message_field(FieldDefinition::new(FIELDNAME, None, DEC, 0, -1), 0);
-    let (result, _) = From::from(buffer);
+    let field_definition: FieldDefinition = FieldDefinition::new(FIELDNAME, None, DEC, 0, -1);
+    let (_, extracted_field) = wf_buffer.extract_message_field(field_definition, 10);
 
     assert_eq!(
         "47", 
-        result,
+        extracted_field.get().as_ref().unwrap(),
         "Extracted message field (dec) should contain the correct value"
     );
 }
+
+#[test]
+fn test_add_field_hex() {
+    let buffer: Vec<u8> = vec![];
+    let mut wf_buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 0); 
+    let field_definition: FieldDefinition = FieldDefinition::new(FIELDNAME, None, HEX, 0, 4);
+    let mut field = Field::new(FIELDNAME, None, HEX, 0, 4);
+
+    let success = field.set("3f8C");
+    assert!(success.is_ok());
+
+    wf_buffer.append_field(&field);
+
+    let (size, _) = wf_buffer.extract_message_field(field_definition, 0);
+
+    assert_eq!(
+        field.bit_length(), 
+        size,
+        "Buffer bit length should be equal to field bit length"
+    );
+
+    let (result, _) = From::from(wf_buffer);
+
+    assert_eq!(
+        "3f8C", 
+        to_hex(&result),
+        "Message field (hex) should be correctly encoded and added"
+    );
+}
+
+#[test]
+fn test_extract_field_hex() {
+    let buffer: Vec<u8> = vec![0x95, 0xDD, 0xFF, 0xE7];
+    let wf_buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 32); 
+
+    let field_definition: FieldDefinition = FieldDefinition::new(FIELDNAME, None, HEX, 0, 2);
+    let (_, extracted_field) = wf_buffer.extract_message_field(field_definition, 9);
+
+    assert_eq!(
+        "bb", 
+        extracted_field.get().as_ref().unwrap(),
+        "Extracted message field (dec) should contain the correct value"
+    );
+
+}
+
+/*public void testExtractFieldHex() throws WfCoreException {
+
+    final byte[] byteArray = {(byte) 0x95, (byte) 0xDD,(byte) 0xFF,(byte) 0xE7};  //  ........ 1|1011 1011 |111
+    WfBinaryBuffer buffer = WfBinaryBuffer.fromByteArray(byteArray);
+    WfMessageField field = WfMessageField.define(FIELDNAME, null, HEX, 0, 2);
+
+    assertEquals("Extracted message field (hex) should contain the correct value", "bb", buffer.extractMessageField(field, 9).get());
+}*/
+
