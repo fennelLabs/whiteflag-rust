d5ff459f865804daf6ae121c4e1d79399a25f0c0
Ed Hertzog <edhertzog@gmail.com>
2022-07-16 22:40:48 -0400

    fixed test_extract_field_utf()

--- src/wf_field/test.rs
+++ src/wf_field/test.rs
@@ -1,4 +1,4 @@
-use crate::wf_codec::encoding::UTF8;
+use crate::wf_codec::encoding::{UTF8, BIN};
 use crate::{wf_buffer::WhiteflagBuffer, wf_field::Field, wf_field::FieldDefinition};
 use crate::wf_buffer::common::{to_hex};
 
@@ -38,20 +38,18 @@ fn test_extract_field_utf() {
  
     let byte_array_1: Vec<u8> = vec![0x95, 0x74, 0x78, 0x74]; 
 
-    let mut buffer: WhiteflagBuffer = WhiteflagBuffer::new(byte_array_1, 32); 
-    let mut field = Field::new(FIELDNAME, None, UTF8, 0, -1);
+    let buffer: WhiteflagBuffer = WhiteflagBuffer::new(byte_array_1, 32); 
+    let field = Field::new(FIELDNAME, None, UTF8, 0, -1);
 
-    let field: FieldDefinition = FieldDefinition::new(FIELDNAME, None, UTF8, 0, -1);
-    let (_, mut extracted_field) = buffer.extract_message_field(field, 8);
+    let field_definition: FieldDefinition = FieldDefinition::new(FIELDNAME, None, UTF8, 0, -1);
+    let (_, extracted_field) = buffer.extract_message_field(field_definition, 8);
 
-    //let data = extracted_field.extract_message_field(wf_buffer, 8, 0);
     assert_eq!(
         "txt",
-        //extracted_field.extract_message_field(&wf_buffer, message_buffer_bit_length: usize, start_bit: usize),
+        extracted_field.get().as_ref().unwrap(),
         "Extracted message field (UTF) should contain the correct value"
     );
-    /* Verify */
-    //assertEquals("Extracted message field (UTF) should contain the correct value", "txt", buffer.extractMessageField(field, 8).get());    
+    
 }
 
 #[test]
@@ -59,20 +57,27 @@ fn test_add_field_bin_1() {
     let buffer: Vec<u8> = vec![];
     let mut buffer: WhiteflagBuffer = WhiteflagBuffer::new(buffer, 0); 
 
-    let mut field = Field::new(FIELDNAME, None, UTF8, 0, -1);
+    let mut field = Field::new(FIELDNAME, None, BIN, 0, -1);
 
     let success = field.set("01");
     assert!(success.is_ok());
 
     buffer.append_field(&field);
 
-    let (size, _) = buffer.extract_message_field(FieldDefinition::new(FIELDNAME, None, UTF8, 0, -1), 0);
+    let (size, extracted_field) = buffer.extract_message_field(FieldDefinition::new(FIELDNAME, None, BIN, 0, -1), 0);
 
     assert_eq!(
         field.bit_length(), 
         size,
         "Buffer bit length should be equal to field bit length"
     );
+
+    assert_eq!(
+        "40", 
+        to_hex(&extracted_field.encode().expect("correct string")),
+        "Message field (bin) should be correctly encoded and added"
+    );
+
 }
 
 //@Test
