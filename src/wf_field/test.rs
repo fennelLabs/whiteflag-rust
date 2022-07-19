fcc0362e7253f07b21970a4938e2fc6980c952cd
Ed Hertzog <edhertzog@gmail.com>
2022-07-19 11:31:04 -0400

    conversion of WfBinaryBufferTest.java tests (#12)
    * conversion of testAppendBits2 to test_append_bits_2
    
    * start test_append_bits_3()
    
    * cargo fmt
    
    * generate test_extract_bits_2()
    
    * create test_extract_bits_3()
    
    * test for test_extract_bits_4()
    
    * a bit of refactoring
    
    * some comments
    
    * moving field related tests into new field module
    
    * test_add_field_bin_1()
    
    * fixed test_extract_field_utf()
    
    * test_add_field_bin_1() fixed/finished
    
    * test_add_field_bin_2()
    
    * test_add_field_dec()
    
    * test_add_field_dec() fixed
    
    * fn test_extract_field_dec()
    
    * est_extract_field_hex()
    
    * no longer need option on the Field struct (#20)
    
    * using WhiteflagBuffer for BasicMessage (#21)
    
    * fmt
    
    * fixing up the add_field tests
    
    * fixing up extract tests
    
    * fixing append_bits_3
    
    * fixing append bits test
    
    * fmt
    
    * add field hex from merge
    
    * extract field test from merge
    
    * test fix
    
    * start bit should be 10 according to the unit test in the java codebase
    
    * simplifying API
    
    * test_add_field_date_time()
    
    Co-authored-by: Husky <huskydev@pm.me>

---
+++ src/wf_field/test.rs
@@ -0,0 +1,181 @@
+use crate::wf_buffer::common::to_hex;
+use crate::wf_codec::encoding::{BIN, DATETIME, DEC, HEX, UTF8};
+use crate::{wf_buffer::WhiteflagBuffer, wf_field::Field, wf_field::FieldDefinition};
+
+const FIELDNAME: &str = "TESTFIELD";
+
+#[test]
+fn test_add_field_utf() {
+    let mut buffer: WhiteflagBuffer = WhiteflagBuffer::default();
+
+    let field = FieldDefinition::new(FIELDNAME, None, UTF8, 0, -1)
+        .set("text")
+        .expect("invalid");
+
+    buffer.append_field(&field);
+
+    assert_eq!(
+        field.bit_length(),
+        buffer.bit_length(),
+        "Buffer bit length should be equal to field bit length"
+    );
+
+    assert_eq!(
+        "74657874",
+        to_hex(buffer.as_ref()),
+        "Message field (UTF) should be correctly encoded and added"
+    );
+}
+
+#[test]
+fn test_extract_field_utf() {
+    let buffer: WhiteflagBuffer = vec![0x95, 0x74, 0x78, 0x74].into();
+    let def = FieldDefinition::new(FIELDNAME, None, UTF8, 0, -1);
+    let field = buffer.extract_message_field(def, 8);
+
+    assert_eq!(
+        "txt",
+        field.get(),
+        "Extracted message field (UTF) should contain the correct value"
+    );
+}
+
+#[test]
+fn test_add_field_bin_1() {
+    let mut buffer: WhiteflagBuffer = WhiteflagBuffer::default();
+    let field = FieldDefinition::new(FIELDNAME, None, BIN, 0, -1)
+        .set("01")
+        .expect("invalid");
+
+    buffer.append_field(&field);
+
+    assert_eq!(
+        field.bit_length(),
+        buffer.bit_length(),
+        "Buffer bit length should be equal to field bit length"
+    );
+
+    assert_eq!(
+        "40",
+        to_hex(buffer.as_ref()),
+        "Message field (bin) should be correctly encoded and added"
+    );
+}
+
+#[test]
+fn test_add_field_bin_2() {
+    let mut buffer: WhiteflagBuffer = WhiteflagBuffer::default();
+    let field = FieldDefinition::new(FIELDNAME, None, BIN, 0, 3)
+        .set("101")
+        .expect("invalid");
+
+    buffer.append_field(&field);
+
+    assert_eq!(
+        field.bit_length(),
+        buffer.bit_length(),
+        "Buffer bit length should be equal to field bit length"
+    );
+
+    assert_eq!(
+        "a0",
+        to_hex(buffer.as_ref()),
+        "Message field (bin) should be correctly encoded and added"
+    );
+}
+
+#[test]
+fn test_add_field_dec() {
+    let mut buffer: WhiteflagBuffer = WhiteflagBuffer::default();
+    let field = FieldDefinition::new(FIELDNAME, None, DEC, 0, 4)
+        .set("1478")
+        .expect("invalid");
+
+    buffer.append_field(&field);
+
+    assert_eq!(
+        field.bit_length(),
+        buffer.bit_length(),
+        "Buffer bit length should be equal to field bit length"
+    );
+
+    assert_eq!(
+        "1478",
+        to_hex(buffer.as_ref()),
+        "Message field (bin) should be correctly encoded and added"
+    );
+}
+
+#[test]
+fn test_extract_field_dec() {
+    let buffer: WhiteflagBuffer = vec![0x95, 0x91, 0xFF, 0xE7].into();
+    let def = FieldDefinition::new(FIELDNAME, None, DEC, 0, 2);
+
+    let field = buffer.extract_message_field(def, 10);
+
+    assert_eq!(
+        "47",
+        field.get(),
+        "Extracted message field (dec) should contain the correct value"
+    );
+}
+
+#[test]
+fn test_add_field_hex() {
+    let mut buffer = WhiteflagBuffer::default();
+
+    let field = FieldDefinition::new(FIELDNAME, None, HEX, 0, 4)
+        .set("3f8C")
+        .expect("invalid");
+
+    buffer.append_field(&field);
+
+    assert_eq!(
+        field.bit_length(),
+        buffer.bit_length(),
+        "Buffer bit length should be equal to field bit length"
+    );
+
+    assert_eq!(
+        "3f8c",
+        to_hex(buffer.as_ref()),
+        "Message field (hex) should be correctly encoded and added"
+    );
+}
+
+#[test]
+fn test_extract_field_hex() {
+    let buffer: WhiteflagBuffer = vec![0x95, 0xDD, 0xFF, 0xE7].into();
+
+    let def: FieldDefinition = FieldDefinition::new(FIELDNAME, None, HEX, 0, 2);
+    let field = buffer.extract_message_field(def, 9);
+
+    assert_eq!(
+        "bb",
+        field.get(),
+        "Extracted message field (dec) should contain the correct value"
+    );
+}
+
+#[test]
+fn test_add_field_date_time() {
+    let mut buffer = WhiteflagBuffer::default();
+
+    let field = FieldDefinition::new(FIELDNAME, None, DATETIME, 0, -1)
+        .set("2020-07-01T21:42:23Z")
+        .expect("invalid");
+
+    buffer.append_field(&field);
+
+    assert_eq!(
+        field.bit_length(),
+        buffer.bit_length(),
+        "Buffer bit length should be equal to field bit length"
+    );
+
+    assert_eq!(
+        "20200701214223",
+        to_hex(buffer.as_ref()),
+        "Message field (hex) should be correctly encoded and added"
+    );
+}
