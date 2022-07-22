7a403a43cb5cdd02aed65c9deea113759e5b49bd
Sean Batzel <romulus108@protonmail.com>
2022-07-22 00:48:58 -0400

    Getting some testing and general logic validation locked down.

--- src/wf_crypto/ecdh_keypair.rs
+++ src/wf_crypto/ecdh_keypair.rs
@@ -9,21 +9,25 @@ use x25519_dalek::{PublicKey, SharedSecret, StaticSecret};
 ///
 /// Whiteflag Specification 5.2.2 Key Agreement
 
-trait WfECDHKeyPair {
+pub trait WfECDHKeyPair {
     /// Returns the public key of this key pair
     fn get_public_key(&self) -> PublicKey;
+    fn get_shared_secret(&self) -> Option<&SharedSecret>;
 
     /// Returns the raw public key of the ECDH key pair
     fn get_raw_public_key(&self) -> [u8; 32];
 
     /// Calculates the negotiated shared key with an originator
-    fn negotiate_key(&self, public_key: [u8; 32]) -> Vec<u8>;
+    fn negotiate_key(&mut self, public_key: [u8; 32]) -> Vec<u8>;
 
     /// Creates a new random ECDH key with the curve specified for Whiteflag key negotiation
-    fn create_keypair(&self) -> &WhiteflagECDHKeyPair;
+    fn create_keypair() -> WhiteflagECDHKeyPair;
 
     /// Creates an ECDH key pair from an existing private key with the curve specified for Whiteflag key negotiation
-    fn create_keypair_from_secret(&self, secret: StaticSecret) -> &WhiteflagECDHKeyPair;
+    fn create_keypair_from_secret(secret: StaticSecret) -> WhiteflagECDHKeyPair;
+
+    /// Private helper function to prevent rewriting.
+    fn _get_keypair_with_secret(secret: StaticSecret) -> WhiteflagECDHKeyPair;
 
     /// Creates an ECDH public key object from a byte array
     fn create_public_key_from_raw(raw_public_key: [u8; 32]) -> PublicKey;
@@ -32,20 +36,20 @@ trait WfECDHKeyPair {
     fn create_private_key_from_raw(raw_private_key: [u8; 32]) -> StaticSecret;
 }
 
-struct WhiteflagECDHKeyPair {
+pub struct WhiteflagECDHKeyPair {
     /// Main key pair properties
     session_secret: StaticSecret,
     public_key: PublicKey,
-    shared_secret: SharedSecret,
+    shared_secret: Option<SharedSecret>,
 }
 
 /// Constructs a new Whiteflag ECDH key pair
-fn generate_keypair() -> WhiteflagECDHKeyPair {
+pub fn generate_wfkeypair() -> WhiteflagECDHKeyPair {
     WhiteflagECDHKeyPair::create_keypair()
 }
 
 /// Constructs a new Whiteflag ECDH key pair from an existing private key
-fn generate_keypair_from_key(private_key: StaticSecret) -> WhiteflagECDHKeyPair {
+pub fn generate_wfkeypair_from_key(private_key: StaticSecret) -> WhiteflagECDHKeyPair {
     WhiteflagECDHKeyPair::create_keypair_from_secret(private_key)
 }
 
@@ -55,30 +59,43 @@ impl WfECDHKeyPair for WhiteflagECDHKeyPair {
         self.public_key
     }
 
+    fn get_shared_secret(&self) -> Option<&SharedSecret> {
+        self.shared_secret.as_ref()
+    }
+
     /// Returns the raw public key of the ECDH key pair
     fn get_raw_public_key(&self) -> [u8; 32] {
         *self.get_public_key().as_bytes()
     }
 
     /// Calculates the negotiated shared key with an originator
-    fn negotiate_key(&self, public_key: [u8; 32]) -> Vec<u8> {
-        get_shared_secret(self.session_secret, &PublicKey::from(public_key)).as_bytes().to_vec()
+    fn negotiate_key(&mut self, public_key: [u8; 32]) -> Vec<u8> {
+        let secret = get_shared_secret(self.session_secret.clone(), &PublicKey::from(public_key));
+        self.shared_secret = Some(secret);
+        self.shared_secret.as_ref().unwrap().as_bytes().to_vec()
     }
 
     /// Creates a new random ECDH key with the curve specified for Whiteflag key negotiation
-    fn create_keypair() -> &WhiteflagECDHKeyPair {
+    fn create_keypair() -> WhiteflagECDHKeyPair {
         let secret: StaticSecret = get_session_secret();
-        let public_key = get_session_public_key(&secret);
-        let session_secret = secret;
-        let pair = WhiteflagECDHKeyPair { session_secret, public_key, shared_secret: todo!() }
-        &pair
+        let pair = Self::_get_keypair_with_secret(secret);
+        pair
     }
 
     /// Creates an ECDH key pair from an existing private key with the curve specified for Whiteflag key negotiation
-    fn create_keypair_from_secret(&self, secret: StaticSecret) -> &WhiteflagECDHKeyPair {
-        self.public_key = get_session_public_key(&secret);
-        self.session_secret = secret;
-        self
+    fn create_keypair_from_secret(secret: StaticSecret) -> WhiteflagECDHKeyPair {
+        let pair = Self::_get_keypair_with_secret(secret);
+        pair
+    }
+
+    fn _get_keypair_with_secret(secret: StaticSecret) -> WhiteflagECDHKeyPair {
+        let public_key = get_session_public_key(&secret);
+        let session_secret = secret;
+        WhiteflagECDHKeyPair {
+            session_secret,
+            public_key,
+            shared_secret: None,
+        }
     }
 
     /// Creates an ECDH public key object from a byte array
--- src/wf_crypto/keypair_tests.rs
+++ src/wf_crypto/keypair_tests.rs
@@ -1,8 +1,18 @@
-#[test]
-fn test_generate_keypair() {}
+use fennel_lib::{get_session_secret, get_session_public_key};
+use super::ecdh_keypair::{generate_wfkeypair, generate_wfkeypair_from_key, WfECDHKeyPair};
 
 #[test]
-fn test_generate_keypair_from_key() {}
+fn test_generate_keypair() {
+    generate_wfkeypair();
+}
+
+#[test]
+fn test_generate_keypair_from_key() {
+    let static_secret = get_session_secret();
+    let public_key = get_session_public_key(&static_secret);
+    let keypair = generate_wfkeypair_from_key(static_secret);
+    assert_eq!(public_key.as_bytes(), keypair.get_public_key().as_bytes());
+}
 
 #[test]
 fn test_get_public_key() {}
