36edbef7818ab26687ae42f99b398db58cb73631
Husky <huskydev@pm.me>
2022-07-25 19:00:26 -0400

    adding authentication structs

---
+++ src/wf_auth/mod.rs
@@ -0,0 +1,34 @@
+use crate::{wf_buffer::common::decode_from_hexadecimal, wf_crypto::crypto_util::hkdf};
+
+struct WhiteflagAuthMethod {
+    value: String,
+    length: u8,
+    hkdf_salt: Vec<u8>,
+}
+
+impl WhiteflagAuthMethod {
+    pub fn new(value: String, length: u8, hkdf_salt: Vec<u8>) -> WhiteflagAuthMethod {
+        WhiteflagAuthMethod {
+            value,
+            length,
+            hkdf_salt,
+        }
+    }
+}
+
+struct WhiteflagAuthToken {
+    token: Vec<u8>,
+    method: WhiteflagAuthMethod,
+}
+
+impl WhiteflagAuthToken {
+    pub fn get_verification_data<T: AsRef<str>>(&self, context: T) -> String {
+        let (buffer, _) = decode_from_hexadecimal(context);
+        hex::encode(hkdf(
+            &self.token,
+            &self.method.hkdf_salt,
+            buffer,
+            &self.method.length,
+        ))
+    }
+}
