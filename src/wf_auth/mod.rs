db0c0b43dd3c213b6e7352de09ef7255dec0d4e3
Husky <huskydev@pm.me>
2022-07-26 00:35:49 -0400

    using the hkdf api from util

--- src/wf_auth/mod.rs
+++ src/wf_auth/mod.rs
@@ -1,13 +1,13 @@
-use crate::{wf_buffer::common::decode_from_hexadecimal, wf_crypto::crypto_util::hkdf};
+use crate::{wf_buffer::common::decode_from_hexadecimal, wf_crypto::hkdf};
 
 struct WhiteflagAuthMethod {
     value: String,
-    length: u8,
+    length: usize,
     hkdf_salt: Vec<u8>,
 }
 
 impl WhiteflagAuthMethod {
-    pub fn new(value: String, length: u8, hkdf_salt: Vec<u8>) -> WhiteflagAuthMethod {
+    pub fn new(value: String, length: usize, hkdf_salt: Vec<u8>) -> WhiteflagAuthMethod {
         WhiteflagAuthMethod {
             value,
             length,
@@ -24,11 +24,12 @@ struct WhiteflagAuthToken {
 impl WhiteflagAuthToken {
     pub fn get_verification_data<T: AsRef<str>>(&self, context: T) -> String {
         let (buffer, _) = decode_from_hexadecimal(context);
+
         hex::encode(hkdf(
             &self.token,
             &self.method.hkdf_salt,
-            buffer,
-            &self.method.length,
+            &buffer,
+            self.method.length,
         ))
     }
 }
