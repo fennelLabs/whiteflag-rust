e9e1bf8d36fc47c5820c3a45206bb3e39014f812
Husky <huskydev@pm.me>
2022-07-26 00:42:40 -0400

    avoiding unwrap()

--- src/wf_crypto/mod.rs
+++ src/wf_crypto/mod.rs
@@ -7,8 +7,6 @@ mod crypto_util_tests;
 mod crypto_util;
 pub mod ecdh_keypair;
 
-pub fn hkdf(ikm: &[u8], salt: &[u8], info: &[u8], length: usize) -> Vec<u8> {
-    crypto_util::SimpleWhiteflagHkdf::<sha2::Sha256>::new(ikm, salt)
-        .expand(info, length)
-        .unwrap()
+pub fn hkdf(ikm: &[u8], salt: &[u8], info: &[u8], length: usize) -> Result<Vec<u8>, ()> {
+    crypto_util::SimpleWhiteflagHkdf::<sha2::Sha256>::new(ikm, salt).expand(info, length)
 }
