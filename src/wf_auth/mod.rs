8fe04fc51347c17e4a1213f38ecda43763ae3bd3
Husky <huskydev@pm.me>
2022-07-27 21:08:57 -0400

    preferring to use [u8] in most places

--- src/wf_auth/mod.rs
+++ src/wf_auth/mod.rs
@@ -1,48 +1,12 @@
-use crate::{error::WhiteflagResult, wf_buffer::HexadecimalString, wf_crypto::hkdf};
-
 mod test;
 
-const preshared_secret: &'static str =
-    "420abc48f5d69328c457d61725d3fd7af2883cad8460976167e375b9f2c14081";
-
-struct WhiteflagAuthMethod {
-    value: AuthenticationMethod,
-    length: usize,
-    hkdf_salt: HexadecimalString,
-}
-
-impl WhiteflagAuthMethod {
-    pub fn new(
-        value: AuthenticationMethod,
-        length: usize,
-        hkdf_salt: HexadecimalString,
-    ) -> WhiteflagAuthMethod {
-        WhiteflagAuthMethod {
-            value,
-            length,
-            hkdf_salt,
-        }
-    }
-
-    fn get_preshared_token() -> Self {
-        WhiteflagAuthMethod {
-            value: AuthenticationMethod::PRESHARED_TOKEN,
-            length: 32,
-            hkdf_salt: preshared_secret.to_string().into(),
-        }
-    }
-}
-
-struct WhiteflagAuthToken {
-    token: Vec<u8>,
-    method: WhiteflagAuthMethod,
-}
+use crate::wf_crypto::hkdf;
 
 /// Whiteflag specifies two methods for authentication
 /// (4.3.4.2 https://standard.whiteflagprotocol.org/v1/)
 /// - (1) internet resource
 /// - (2) pre shared token
-enum AuthenticationMethod {
+pub enum AuthenticationMethod {
     INTERNET_RESOURCE,
     PRESHARED_TOKEN,
 }
@@ -56,19 +20,61 @@ impl AuthenticationMethod {
     }
 }
 
+/// hex = 420abc48f5d69328c457d61725d3fd7af2883cad8460976167e375b9f2c14081
+const preshared_secret: [u8; 32] = [
+    66, 10, 188, 72, 245, 214, 147, 40, 196, 87, 214, 23, 37, 211, 253, 122, 242, 136, 60, 173,
+    132, 96, 151, 97, 103, 227, 117, 185, 242, 193, 64, 129,
+];
+
+struct WhiteflagAuthMethod {
+    value: AuthenticationMethod,
+    length: usize,
+    hkdf_salt: Vec<u8>,
+}
+
+impl WhiteflagAuthMethod {
+    pub fn new(
+        value: AuthenticationMethod,
+        length: usize,
+        hkdf_salt: Vec<u8>,
+    ) -> WhiteflagAuthMethod {
+        WhiteflagAuthMethod {
+            value,
+            length,
+            hkdf_salt,
+        }
+    }
+
+    pub fn get_preshared_token() -> Self {
+        WhiteflagAuthMethod {
+            value: AuthenticationMethod::PRESHARED_TOKEN,
+            length: 32,
+            hkdf_salt: preshared_secret.to_vec(),
+        }
+    }
+}
+
+struct WhiteflagAuthToken {
+    token: Vec<u8>,
+    method: WhiteflagAuthMethod,
+}
+
 impl WhiteflagAuthToken {
-    pub fn new(secret: HexadecimalString) -> WhiteflagAuthToken {
-        let token: Vec<u8> = secret.try_into().map_err(|e| panic!("{}", e)).unwrap();
+    pub fn new<T: Into<Vec<u8>>>(secret: T) -> WhiteflagAuthToken {
         WhiteflagAuthToken {
-            token,
+            token: secret.into(),
             method: WhiteflagAuthMethod::get_preshared_token(),
         }
     }
 
-    pub fn get_verification_data<T: AsRef<str>>(&self, context: T) -> WhiteflagResult<String> {
-        let info = hex::decode(context.as_ref())?;
-        let salt: Vec<u8> = (&self.method.hkdf_salt).try_into()?;
-        let result = hkdf(&self.token, &salt, &info, self.method.length).unwrap();
-        Ok(hex::encode(result))
+    pub fn get_verification_data<T: AsRef<[u8]>>(&self, context: T) -> String {
+        let result = hkdf(
+            &self.token,
+            &self.method.hkdf_salt,
+            context.as_ref(),
+            self.method.length,
+        )
+        .unwrap();
+        hex::encode(result)
     }
 }
--- src/wf_auth/test.rs
+++ src/wf_auth/test.rs
@@ -3,16 +3,21 @@ use super::WhiteflagAuthToken;
 #[test]
 fn test_auth_token() {
     /* Setup */
-    let secret = "000102030405060708090a0b0c";
-    let context = "6fdb25dc394d5a437d88f15b459406ac6db8b386a49dbfc38c";
+    let secret = hex::decode("000102030405060708090a0b0c").unwrap();
+    let context = hex::decode("6fdb25dc394d5a437d88f15b459406ac6db8b386a49dbfc38c").unwrap();
     let verification_data = "a951cb35881ee7f78b05f8476a2193de4556455d48ffcfebcfc8938f4a37a70f";
-    let token = WhiteflagAuthToken::new(secret.into());
+    let token = WhiteflagAuthToken::new(secret);
 
     /* Verify */
     //assertEquals("Authentication token should have the correct authentication indicator", TOKEN_PRESHARED.fieldValue, token.method.fieldValue);
     assert_eq!(
         verification_data,
-        token.get_verification_data(context).unwrap(),
+        token.get_verification_data(context),
         "Authentication token should give the correct verification data"
     );
+
+    println!(
+        "{:?}",
+        hex::decode("420abc48f5d69328c457d61725d3fd7af2883cad8460976167e375b9f2c14081").unwrap()
+    )
 }
