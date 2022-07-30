fa25c5b602cd8f690c5d006ce1515e400548fec6
Husky <huskydev@pm.me>
2022-07-30 15:44:59 -0400

    assigning each definition to a variable for future use

--- src/wf_field/definitions.rs
+++ src/wf_field/definitions.rs
@@ -46,37 +46,46 @@ pub fn test_message_code() -> FieldDefinition {
 }
 
 pub fn generic_header_fields() -> [FieldDefinition; 7] {
+    let prefix = FieldDefinition::new("Prefix", Regex::new("^WF$").ok(), UTF8, 0, 2);
+    let version = FieldDefinition::new("Version", Regex::new("^[A-Z0-9]{1}$").ok(), UTF8, 2, 3); //"(?=1)^[A-Z0-9]{1}$"
+    let encryption_indicator = FieldDefinition::new(
+        "EncryptionIndicator",
+        Regex::new("^[A-Z0-9]{1}$").ok(), //"(?=0|1|2)^[A-Z0-9]{1}$"
+        UTF8,
+        3,
+        4,
+    );
+    let duress_indicator =
+        FieldDefinition::new("DuressIndicator", Regex::new("^[0-1]{1}$").ok(), BIN, 4, 5);
+    let message_code = message_code();
+    let reference_indicator = FieldDefinition::new(
+        "ReferenceIndicator",
+        Regex::new(
+            ["^", HEX.charset, "{1}$"] //"(?=0|1|2|3|4|5|6|7|8|9)^", HEX.charset, "{1}$"
+                .concat()
+                .as_str(),
+        )
+        .ok(),
+        HEX,
+        6,
+        7,
+    );
+    let referenced_message = FieldDefinition::new(
+        "ReferencedMessage",
+        Regex::new(["^", HEX.charset, "{64}$"].concat().as_str()).ok(),
+        HEX,
+        7,
+        71,
+    );
+
     [
-        FieldDefinition::new("Prefix", Regex::new("^WF$").ok(), UTF8, 0, 2),
-        FieldDefinition::new("Version", Regex::new("^[A-Z0-9]{1}$").ok(), UTF8, 2, 3), //"(?=1)^[A-Z0-9]{1}$"
-        FieldDefinition::new(
-            "EncryptionIndicator",
-            Regex::new("^[A-Z0-9]{1}$").ok(), //"(?=0|1|2)^[A-Z0-9]{1}$"
-            UTF8,
-            3,
-            4,
-        ),
-        FieldDefinition::new("DuressIndicator", Regex::new("^[0-1]{1}$").ok(), BIN, 4, 5),
-        message_code(),
-        FieldDefinition::new(
-            "ReferenceIndicator",
-            Regex::new(
-                ["^", HEX.charset, "{1}$"] //"(?=0|1|2|3|4|5|6|7|8|9)^", HEX.charset, "{1}$"
-                    .concat()
-                    .as_str(),
-            )
-            .ok(),
-            HEX,
-            6,
-            7,
-        ),
-        FieldDefinition::new(
-            "ReferencedMessage",
-            Regex::new(["^", HEX.charset, "{64}$"].concat().as_str()).ok(),
-            HEX,
-            7,
-            71,
-        ),
+        prefix,
+        version,
+        encryption_indicator,
+        duress_indicator,
+        message_code,
+        reference_indicator,
+        referenced_message,
     ]
 }
 
