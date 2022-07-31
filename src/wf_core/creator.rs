5636ac8bdf9deb7188050f7ef94cfa51c8850516
Sean Batzel <romulus108@protonmail.com>
2022-07-30 22:05:11 -0400

    WhiteflagMessageCreator Port.

--- src/wf_core/creator.rs
+++ src/wf_core/creator.rs
@@ -12,6 +12,10 @@ pub fn encode<T: FieldValue>(fields: &[T]) -> String {
     hex::encode(message_encoded)
 }
 
+struct WhiteflagMessageCreator {}
+
+impl WhiteflagMessageCreator {}
+
 /**
  * Decodes an encoded Whiteflag message and creates a new Whiteflag base message object
  * @since 1.1
