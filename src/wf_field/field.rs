61d25e163f6efca9b3f6a2a97a86269b7ebcef6a
Husky <huskydev@pm.me>
2022-08-07 16:21:12 -0400

    expanding options for creating a Field struct

--- src/wf_field/field.rs
+++ src/wf_field/field.rs
@@ -6,11 +6,25 @@ use crate::wf_core::error::{WhiteflagError, WhiteflagResult};
 pub struct Field {
     pub definition: FieldDefinition,
     value: String,
+    name: String,
 }
 
 impl Field {
     pub fn new(definition: FieldDefinition, value: String) -> Field {
-        Field { definition, value }
+        let name = definition.get_name();
+        Field::new_with_name(definition, value, name)
+    }
+
+    pub fn new_with_name(definition: FieldDefinition, value: String, name: String) -> Field {
+        Field {
+            definition,
+            value,
+            name,
+        }
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name
     }
 
     /* pub fn get(&self, data: Vec<String>) -> WhiteflagResult<String> {
