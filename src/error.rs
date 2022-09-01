30e304bbb8cbe6b63c5c9d5685cb0881d00211d3
isaacadams <isaacdadams@pm.me>
2022-09-01 00:19:44 -0400

    reducing dependencies

--- src/error.rs
+++ src/error.rs
@@ -7,10 +7,6 @@ pub enum WhiteflagError {
     Serde(serde_json::Error),
     #[error("issue decoding hex string")]
     HexDecode(#[from] hex::FromHexError),
-    #[error("invalid psuedo random key length")]
-    HkdfInput(hkdf::InvalidPrkLength),
-    #[error("invalid output length")]
-    HkdfOutput(hkdf::InvalidLength),
     #[error(transparent)]
     Other(#[from] Box<dyn std::error::Error>),
 }
