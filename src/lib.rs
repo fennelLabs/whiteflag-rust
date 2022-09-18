98236c0a85dc21168dc9f7c1811176be28321de8
Ed Hertzog <edhertzog@gmail.com>
2022-09-17 21:26:16 -0400

    clippy: redundant closure

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,14 @@ mod wf_json;
 
 pub fn encode_from_json<T: AsRef<str>>(json: T) -> Result<String, WhiteflagError> {
     let message: wf_json::WhiteflagFieldValues =
-        serde_json::from_str(json.as_ref()).map_err(|e| WhiteflagError::Serde(e))?;
+        serde_json::from_str(json.as_ref()).map_err(WhiteflagError::Serde)?;
 
     Ok(wf_core::encode(&message.fields))
 }
 
 pub fn decode_from_hex<T: AsRef<str>>(hex: T) -> Result<String, WhiteflagError> {
     let message = wf_core::decode(hex);
-    let json = serde_json::to_string(&message).map_err(|e| WhiteflagError::Serde(e))?;
+    let json = serde_json::to_string(&message).map_err(WhiteflagError::Serde)?;
 
     Ok(json)
 }
