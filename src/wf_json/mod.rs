0d2d371c48cf7bd58f46b7f8a041976549b10eb4
Ed Hertzog <edhertzog@gmail.com>
2022-09-17 21:30:10 -0400

    clippy: redundant closure

--- src/wf_json/mod.rs
+++ src/wf_json/mod.rs
@@ -11,7 +11,7 @@ use crate::{error::WhiteflagError, wf_core::message::Message};
 impl Message {
     pub fn deserialize_from_json<T: AsRef<str>>(json: T) -> Result<Self, WhiteflagError> {
         let message: WhiteflagFieldValues =
-            serde_json::from_str(json.as_ref()).map_err(|e| WhiteflagError::Serde(e))?;
+            serde_json::from_str(json.as_ref()).map_err(WhiteflagError::Serde)?;
         Ok(Message::compile(message.fields.as_ref()))
     }
 }
