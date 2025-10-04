1bfd6aa3c1be497fb900a6554f5defde4c9cdfcd
CorruptedAesthetic <53838755+CorruptedAesthetic@users.noreply.github.com>
2025-10-04 19:06:26 -0400

    Fix clippy warning in wf_codec: use is_multiple_of() instead of manual implementation

--- wf_codec/src/binary.rs
+++ wf_codec/src/binary.rs
@@ -6,7 +6,7 @@ use wf_common::constants::BYTE;
 pub fn encode_from_binary<T: AsRef<str>>(binary_str: T) -> Vec<u8> {
     let binary = binary_str.as_ref();
     let bit_length: usize = binary.len();
-    let byte_length: usize = (bit_length / BYTE) + (if bit_length % BYTE == 0 { 0 } else { 1 });
+    let byte_length: usize = (bit_length / BYTE) + (if bit_length.is_multiple_of(BYTE) { 0 } else { 1 });
 
     let mut buffer = vec![0; byte_length];
 
